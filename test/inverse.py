72ab5d79143cc95ec06a9f2717ed25cfef1fe977
hanqingguo <18523978636@163.com>
2020-06-22 20:57:46 -0400

    hanqing-v1

--- test/inverse.py
+++ test/inverse.py
@@ -6,8 +6,6 @@ import scipy
 from scipy import signal
 from IPython.display import Audio
 
-
-
 sa = librosa.load("test_data/speakerA.wav")[0]
 sb = librosa.load("test_data/speakerB.wav")[0]
 print(librosa.load("test_data/speakerA.wav")[1])
@@ -21,21 +19,21 @@ plt.subplot(312)
 plt.plot(t, sb)
 plt.title("Speaker B")
 plt.subplot(313)
-plt.plot(t, sa+sb)
+plt.plot(t, sa + sb)
 plt.title("Speaker A+B")
 plt.show()
 
 fsa = librosa.stft(sa)
 fsb = librosa.stft(sb)
-fboth = librosa.stft(sa+sb)
+fboth = librosa.stft(sa + sb)
 plt.subplot(311)
-librosa.display.specshow(librosa.amplitude_to_db(fsa, ref=np.max),y_axis='log', x_axis='time')
+librosa.display.specshow(librosa.amplitude_to_db(fsa, ref=np.max), y_axis='log', x_axis='time')
 plt.title("Speaker A")
 plt.subplot(312)
-librosa.display.specshow(librosa.amplitude_to_db(fsb, ref=np.max),y_axis='log', x_axis='time')
+librosa.display.specshow(librosa.amplitude_to_db(fsb, ref=np.max), y_axis='log', x_axis='time')
 plt.title("Speaker B")
 plt.subplot(313)
-librosa.display.specshow(librosa.amplitude_to_db(fboth, ref=np.max),y_axis='log', x_axis='time')
+librosa.display.specshow(librosa.amplitude_to_db(fboth, ref=np.max), y_axis='log', x_axis='time')
 plt.title("Speaker A+B")
 plt.show()
 
