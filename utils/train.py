e91838bf68ee0e115f8e442daae3855b54d5e32f
Liecn <lichenni@msu.edu>
2020-07-03 00:54:36 -0400

    add my own power loss

--- utils/train.py
+++ utils/train.py
@@ -90,12 +90,12 @@ def train(args, pt_dir, chkpt_path, trainloader, testloader, writer, logger, hp,
                 # Power-law compression
                 else: 
                     magnitude_loss = criterion(
-                        torch.pow(torch.abs(output), hp.audio.power), 
-                        torch.pow(torch.abs(target_mag), hp.audio.power),
+                        torch.abs(torch.pow(output, hp.audio.power)), 
+                        torch.abs(torch.pow(target_mag, hp.audio.power)),
                     )
                     complex_loss = criterion(
-                        torch.pow(torch.clamp(output, min=0.0), hp.audio.power), 
-                        torch.pow(torch.clamp(target_mag, min=0.0), hp.audio.power),
+                        torch.pow(output, hp.audio.power), 
+                        torch.pow(target_mag, hp.audio.power),
                     )
                     loss = magnitude_loss + complex_loss * hp.train.complex_loss_ratio
                 
