8d3919d6874f78f3fad986c5a6248ccd5bf7918f
Liecn <lichenni@msu.edu>
2020-07-28 17:11:58 -0400

    remove py

--- inference_matlab.py
+++ inference_matlab.py
@@ -8,9 +8,24 @@ from utils.audio import Audio
 from utils.hparams import HParam
 from model.model import VoiceFilter
 from model.embedder import SpeechEmbedder
+import warnings
+warnings.filterwarnings("ignore")
 
+def search(words):
+    """Return list of words containing 'son'"""
+    newlist = [w for w in words if 'son' in w]
+    return newlist
 
 def main(args):
+    args = {
+        "config": 'config/config.yaml',
+        "embedder_path": 'model/embedder.pt',
+        "checkpoint_path": 'enhance_my_voice/chkpt_201000.pt',
+        "mixed_file": 'utils/speakerA.wav',
+        "reference_file": 'utils/speakerA.wav',
+        "out_dir": 'output',
+    }
+
     hp = HParam(args['config'])
 
     with torch.no_grad():
@@ -46,3 +61,29 @@ def main(args):
         # out_path = os.path.join(args['out_dir'], 'result.wav')
         # librosa.output.write_wav(out_path, est_wav, sr=16000)
         return audio.spec2wav(est_mag, phase)
+if __name__ == '__main__':
+
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('-c', '--config', type=str, default='config/config-medium.yaml',
+    #                     help="yaml file for configuration")
+    # parser.add_argument('-e', '--embedder_path', type=str, default='model/embedder.pt',
+    #                     help="path of embedder model pt file")
+    # parser.add_argument('--checkpoint_path', type=str, default=None,
+    #                     help="path of checkpoint pt file")
+    # parser.add_argument('-m', '--mixed_file', type=str, default='v3.2.3-oldDataset',
+    #                     help='path of mixed wav file')
+    # parser.add_argument('-r', '--reference_file', type=str, default='utils/speakerA.wav',
+    #                     help='path of reference wav file')
+    # parser.add_argument('-o', '--out_dir', type=str, default='output',
+    #                     help='directory of output')
+
+    args = {
+  "config": 'config/config.yaml',
+  "embedder_path": 'model/embedder.pt',
+  "checkpoint_path": 'enhance_my_voice/chkpt_201000.pt',
+  "mixed_file": 'utils/speakerA.wav',
+  "reference_file": 'utils/speakerA.wav',
+  "out_dir": 'output',
+}
+    ans=main(args)
+    print(ans.shape,ans)
---
+++ mymod.py
@@ -0,0 +1,10 @@
+
+"""Python module demonstrates passing MATLAB types to Python functions"""
+def search():
+    """Return list of words containing 'son'"""
+    return 1
+
+def theend(words):
+    """Append 'The End' to list of words"""
+    words.append('The End')
+    return words
